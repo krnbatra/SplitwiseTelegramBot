e22ded4cdae511229973e7b161d7c73e65198d25
Karan <karandeepbatra@gmail.com>
2020-05-26 20:11:34 +0530

    Change error to be more descriptive

--- bot/error.py
+++ bot/error.py
@@ -1,12 +1,15 @@
 # Telegram API framework core imports
-from telegram import Update
-from telegram.ext import Dispatcher, CallbackContext
+import sys
+import traceback
+
+from telegram import ParseMode
+from telegram.ext import Dispatcher
+from telegram.utils.helpers import mention_html
+
 
 # Helper methods import
-from utils.logger import get_logger
-
-# Init logger
-logger = get_logger(__name__)
+# # Init logger
+# logger = get_logger(__name__)
 
 
 def init(dispatcher: Dispatcher):
@@ -14,6 +17,47 @@ def init(dispatcher: Dispatcher):
     dispatcher.add_error_handler(error)
 
 
-def error(update: Update, context: CallbackContext):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+# def error(update: Update, context: CallbackContext):
+#     """Log Errors caused by Updates."""
+#     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+# this is a general error handler function. If you need more information about specific type of update, add it to the
+# payload in the respective if clause
+
+
+def error(update, context):
+    # add all the dev user_ids in this list. You can also add ids of channels or groups.
+    devs = [353979697]
+    # we want to notify the user of this problem. This will always work, but not notify users if the update is an
+    # callback or inline query, or a poll update. In case you want this, keep in mind that sending the message
+    # could fail
+    if update.effective_message:
+        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+               "My developer(s) will be notified."
+        update.effective_message.reply_text(text)
+    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+    # empty string works fine.
+    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+    # lets try to get as much information from the telegram update as possible
+    payload = ""
+    # normally, we always have an user. If not, its either a channel or a poll update.
+    if update.effective_user:
+        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+    # there are more situations when you don't get a chat
+    if update.effective_chat:
+        payload += f' within the chat <i>{update.effective_chat.title}</i>'
+        if update.effective_chat.username:
+            payload += f' (@{update.effective_chat.username})'
+    # but only one where you have an empty payload by now: A poll (buuuh)
+    if update.poll:
+        payload += f' with the poll id {update.poll.id}.'
+    # lets put this in a "well" formatted text
+    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+           f"</code>"
+    # and send it to the dev(s)
+    for dev_id in devs:
+        context.bot.send_message(dev_id, text, parse_mode=ParseMode.HTML)
+    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
+    raise
