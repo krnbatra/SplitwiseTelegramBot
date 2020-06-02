8487872b366760602ad6c73e7ed4098ad375b730
Karan <karandeepbatra@gmail.com>
2020-06-02 22:42:24 +0530

    Add reconnect command in commands

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 commands = {
     'connect': 'Connects your Splitwise account',
-    'list_expense': 'List expenses from your Splitwise account',
     'create_expense': 'Creates new expense in your Splitwise account',
+    'list_expense': 'List expenses from your Splitwise account',
+    'reconnect': 'reconnect - Reconnect your Splitwise account',
     'settle_expense': 'Settles expense in your Splitwise account'
 }
